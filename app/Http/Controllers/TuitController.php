82f2a98dbf66654a807b0a44bbaef8ac3d33b386
guillermoroman <guillermoroman@outlook.com>
2024-02-01 08:50:21 +0100

    First commit

---
+++ app/Http/Controllers/TuitController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tuit;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class TuitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() :View
+    {
+        //return "Hola mundo!";
+
+        return view('tuits.index', ['tuits' => Tuit::with('user')->latest()->get()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $request->user()->tuits()->create($validated);
+
+        return redirect(route('tuits.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tuit $tuit)
+    {
+        //
+    }
+}
